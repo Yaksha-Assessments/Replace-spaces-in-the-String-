da0d65936933345a49cc41a1cdae1e52aa9e99db
Yaksha-Assessments <yaksha.mcq@gmail.com>
2025-08-07 16:19:45 +0530

    Add files via upload

---
+++ Replace spaces in the String /Requested Files/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+ using namespace std;
+ string replaceSpaces(string S) {
+  string result="'";  
+  //WRITE YOUR LOGIC HERE:  
+    return result;
+ }
+//Do not edit the code below
+ int main(void) {
+  string S;
+  cin>>S;  
+  cout<<replaceSpaces(S);
+  return 0;
+ }
+//Non editable ends here
