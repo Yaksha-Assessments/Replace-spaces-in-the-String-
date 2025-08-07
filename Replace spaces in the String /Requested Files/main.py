978abae8bda1b175105237b9746e3df6b46b88c5
Yaksha-Assessments <yaksha.mcq@gmail.com>
2025-08-07 16:18:59 +0530

    Delete Replace spaces in the String  directory

--- Replace spaces in the String /Requested Files/main.py
+++
@@ -1,9 +0,0 @@
-def replace_spaces(S):
-    result = ""  
-    #WRITE YOUR LOGIC HERE:  
-    return result
-#Do not edit the code below
-if __name__=="__main__":  
-    S = input() 
-    print(replace_spaces(S))
-#Non editable ends here
