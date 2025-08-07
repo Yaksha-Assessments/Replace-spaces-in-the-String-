d97f2dc07d9c1aed9f447f67088a449b2236f91e
Yaksha-Assessments <yaksha.mcq@gmail.com>
2025-08-07 16:17:36 +0530

    Add files via upload

---
+++ Replace spaces in the String /Requested Files/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Test {
+    public static String replaceSpaces(String S) {
+        String result = "";
+        //WRITE YOUR LOGIC HERE:
+
+        return result;
+    }
+
+//Do not edit the code below
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String S = sc.nextLine();
+        sc.close();
+
+        System.out.println(replaceSpaces(S));
+    }
+}
+//Non editable ends here
---
+++ Replace spaces in the String /Requested Files/main.py
@@ -0,0 +1,9 @@
+def replace_spaces(S):
+    result = ""  
+    #WRITE YOUR LOGIC HERE:  
+    return result
+#Do not edit the code below
+if __name__=="__main__":  
+    S = input() 
+    print(replace_spaces(S))
+#Non editable ends here
